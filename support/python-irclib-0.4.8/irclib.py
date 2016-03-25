0702f61d099841bcf940dbf8a94b84fe651f30ef
KpaBap <kpabap@gmail.com>
2016-03-25 15:09:57 -0700

    Update irclib.py

--- support/python-irclib-0.4.8/irclib.py
+++ support/python-irclib-0.4.8/irclib.py
@@ -1456,6 +1456,7 @@ numeric_events = {
     "303": "ison",
     "305": "unaway",
     "306": "nowaway",
+    "307": "whoisregnick",
     "311": "whoisuser",
     "312": "whoisserver",
     "313": "whoisoperator",
