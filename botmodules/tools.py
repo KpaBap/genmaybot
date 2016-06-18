13da4c00908c55421db3efbbc7df273cc09a79ff
KpaBap <kpabap@gmail.com>
2016-06-18 08:49:02 -0700

    Update tools.py

--- botmodules/tools.py
+++ botmodules/tools.py
@@ -1,180 +1,186 @@
-import re, urllib.request, urllib.error, urllib.parse, urllib, json, traceback
-from html.entities import name2codepoint as n2cp
-from bs4 import BeautifulSoup
-import encodings.idna
+# coding=utf-8
+
+import re
+import hashlib
 import datetime
+import sqlite3
 
 
-def __init__(self):
-    google_url.self = self
-
-
-def set_googleapi(line, nick, self, c):
-    google_url.self.botconfig["APIkeys"]["gsearchapi"] = line[11:]
-    with open('genmaybot.cfg', 'w') as configfile:
-        self.botconfig.write(configfile)
-set_googleapi.admincommand = "gsearchapi"
-
-def set_googlecx(line, nick, self, c):
-    google_url.self.botconfig["APIkeys"]["gsearchcx"] = line[10:]
-    with open('genmaybot.cfg', 'w') as configfile:
-        self.botconfig.write(configfile)
-set_googlecx.admincommand = "gsearchcx"
-
-def set_shorturlkey(line, nick, self, c):
-    google_url.self.botconfig["APIkeys"]["shorturlkey"] = line[12:]
-    with open('genmaybot.cfg', 'w') as configfile:
-        self.botconfig.write(configfile)
-set_shorturlkey.admincommand = "shorturlkey"
-
-def decode_htmlentities(string):
-    #decodes things like &amp
-    entity_re = re.compile("&(#?)(x?)(\w+);")
-    return entity_re.subn(substitute_entity, string)[0]
-
-
-def substitute_entity(match):
-  try:
-    ent = match.group(3)
-
-    if match.group(1) == "#":
-        if match.group(2) == '':
-            return chr(int(ent))
-        elif match.group(2) == 'x':
-            return chr(int('0x' + ent, 16))
+def url_parser(self, e):
+    url = re.search(r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>])*\))+(?:\(([^\s()<>])*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))", e.input)
+    #http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+    if url:
+        url = url.group(0)
+        if url[0:4].lower() != "http":
+            url = "http://" + url
+        e.input = url
+        
+        title = url_posted(self,e)
+        return None #url_posted(self, e)
     else:
-        cp = n2cp.get(ent)
+        return None
+url_parser.lineparser = True
 
-        if cp:
-            return chr(cp)
+
+def url_posted(self, e, titlecall=False):
+    url = e.input
+    #checks if the URL is a dupe (if mysql is enabled)
+    #detects if a wikipedia or imdb url is posted and does the appropriate command for it
+
+    repost = ""
+    days = ""
+
+    urlhash = hashlib.sha224(url.encode()).hexdigest()
+    conn = sqlite3.connect("links.sqlite")
+    cursor = conn.cursor()
+    result = cursor.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='links';").fetchone()
+    if not result:
+        cursor.execute('''CREATE TABLE 'links' ("url" tinytext, "hash" char(56) NOT NULL UNIQUE, "reposted" smallint(5) default '0', "timestamp" timestamp NOT NULL default CURRENT_TIMESTAMP);''')
+        cursor.execute('''CREATE INDEX "links_hash" ON 'links' ("hash");''')
+
+    query = "SELECT reposted, timestamp FROM links WHERE hash='%s'" % urlhash
+    result = cursor.execute(query)
+    result = cursor.fetchone()
+    if result and result[0] != 0:
+        repost = "LOL REPOST %s " % (result[0] + 1)
+
+        orig = datetime.datetime.strptime(result[1], "%Y-%m-%d %H:%M:%S")
+        now = datetime.datetime.utcnow()
+        delta = now - orig
+
+        plural = ""
+        if delta.days > 0:
+            if delta.days > 1:
+                plural = "s"
+            days = " (posted %s day%s ago)" % (str(delta.days), plural)
         else:
-            return match.group()
-  except:
-    return ""
-
-
-def remove_html_tags(data):
-    #removes all html tags from a given string
-    p = re.compile(r'<.*?>')
-    return p.sub('', data)
-
-
-def google_url(searchterm, regexstring):
-    searchterm = urllib.parse.quote(searchterm)
-    key = google_url.self.botconfig["APIkeys"]["gsearchapi"]
-    cx = google_url.self.botconfig["APIkeys"]["gsearchcx"]
-    url = 'https://www.googleapis.com/customsearch/v1?key={}&cx={}&q={}'
-    url = url.format(key, cx, searchterm)
-    
-    try:
-        request = urllib.request.Request(url, None, {'Referer': 'http://irc.00id.net'})
-        response = urllib.request.urlopen(request)
-    except urllib.error.HTTPError as err:
-        print(err.read())
-
-    results_json = json.loads(response.read().decode('utf-8'))
-    results = results_json['items']
-
-    for result in results:
-        print(result['link'])
-        m = re.search(regexstring, result['link'])
-        print(m)
-        if (m):
-            url = result['link']
-            url = url.replace('%25', '%')
-            return url
-    return
-
-
-def load_html_from_URL(url, readlength="", returnurl=False):
-    url = fixurl(url)
-    opener = urllib.request.build_opener()
-
-    opener.addheaders = [('User-Agent', "Opera/9.10 (YourMom 8.0)")]
-
-    page = None
-    pagetmp = opener.open(url)
-    if pagetmp.headers['content-type'].find("text") != -1:
-        url = pagetmp.geturl()
-        if readlength:
-            page = pagetmp.read(int(readlength))
-        else:
-            page = pagetmp.read()
-        page = BeautifulSoup(page)
-    opener.close()
-    if returnurl:
-        return page, url
-    return page
-
-
-def shorten_url(url):
-    #goo.gl url shortening service, not used directly but used by some commands
-  try:
-    key = google_url.self.botconfig["APIkeys"]["shorturlkey"]
-    values = json.dumps({'longUrl': url})
-    headers = {'Content-Type': 'application/json'}
-    requestUrl = "https://www.googleapis.com/urlshortener/v1/url?key={}".format(key)
-    req = urllib.request.Request(requestUrl, values.encode(), headers)
-    response = urllib.request.urlopen(req)
-    results = json.loads(response.read().decode('utf-8'))
-    shorturl = results['id']
-    return shorturl
-  except:
-    traceback.print_exc()
-    return ""
-
-
-def fixurl(url):
-    # turn string into unicode
-    if not isinstance(url, str):
-        url = url.decode('utf8')
-
-    # parse it
-    parsed = urllib.parse.urlsplit(url)
-
-    # divide the netloc further
-    hostport = parsed.netloc
-    host, colon2, port = hostport.partition(':')
-
-    hostnames = host.split(".")
-    tmplist = []
-
-    for tmp in hostnames:
-        tmp = encodings.idna.ToASCII(tmp).decode()
-        tmplist.append(tmp)
-    host = ".".join(tmplist)
-
-    scheme = parsed.scheme
-
-    path = '/'.join(  # could be encoded slashes!
-        urllib.parse.quote(urllib.parse.unquote_to_bytes(pce), '')
-        for pce in parsed.path.split('/')
-    )
-    query = urllib.parse.quote(urllib.parse.unquote_to_bytes(parsed.query), '=&?/')
-    fragment = urllib.parse.quote(urllib.parse.unquote_to_bytes(parsed.fragment))
-
-    # put it back together
-    netloc = ''.join((host, colon2, port))
-    return urllib.parse.urlunsplit((scheme, netloc, path, query, fragment))
-
-
-def prettytimedelta(td):
-    seconds = int(td.total_seconds())
-    periods = [('year',        60*60*24*365),
-               ('month',       60*60*24*30),
-               ('day',         60*60*24),
-               ('hour',        60*60),
-               ('minute',      60),
-               ('second',      1)
-               ]
-
-    strings = []
-    for period_name, period_seconds in periods:
-        if seconds > period_seconds:
-            period_value, seconds = divmod(seconds, period_seconds)
-            if period_value == 1:
-                strings.append("%s %s" % (period_value, period_name))
+            hrs = int(round(delta.seconds / 3600.0, 0))
+            if hrs == 0:
+                mins = round(delta.seconds / 60)
+                if mins > 1:
+                    plural = "s"
+                days = " (posted %s minute%s ago)" % (str(mins), plural)
+                if mins == 0:
+                    repost = ""
+                    days = ""
             else:
-                strings.append("%s %ss" % (period_value, period_name))
+                if hrs > 1:
+                    plural = "s"
+                days = " (posted %s hour%s ago)" % (str(hrs), plural)
 
-    return ", ".join(strings)
+    title = ""
+
+    try:
+        wiki = self.bangcommands["!wiki"](self, e, True)
+        title = wiki.output
+    except:
+        pass
+    try:
+        imdb = self.bangcommands["!imdb"](self, e, True)
+        title = imdb.output
+    except:
+        pass
+    try:
+        yt = self.bangcommands["!yt"](self, e, True)
+        if yt:
+            title = yt.output
+    except:
+        pass
+    try:
+        trope = self.bangcommands["!trope"](self, e, True)
+        if trope:
+            title = trope.output
+    except:
+        pass
+
+    if not titlecall:
+        cursor.execute("""UPDATE OR IGNORE links SET reposted=reposted+1 WHERE hash = ?""", [urlhash])
+
+    cursor.execute("""INSERT OR IGNORE INTO links(url, hash) VALUES (?,?)""", (url, urlhash))
+    conn.commit()
+
+    if url.find("imgur.com") != -1 and url.find("/a/") == -1:
+        imgurid = url[url.rfind('/') + 1:]
+        if "." in imgurid:
+            imgurid = imgurid[:imgurid.rfind('.')]
+        url = "http://imgur.com/" + imgurid
+
+    # Ignore strava ride links because Dan said so, fuck modularity, embace tight coupling.
+    if url.find("app.strava.com/activities") != -1 or url.find("www.strava.com/activities") != -1:
+        return None
+    if not title:
+        title = get_title(self, e, url)
+
+    if title:
+        if title.find("imgur: the simple") != -1:
+            title = ""
+        title = title.replace("\n", " ")
+        title = re.sub('\s+', ' ', title)
+        title = re.sub('(?i)whatsisname', '', title)
+
+
+
+        e.output = "%s%s%s" % (repost, title, days)
+
+    conn.close()
+
+    return e
+
+
+def get_title(self, e, url):
+    length = 51200
+    if url.find("amazon.") != -1:
+        length = 100096  # because amazon is coded like shit
+    page = self.tools["load_html_from_URL"](url, length)
+    title = ""
+
+    if page and page.find('meta', attrs={'name': "generator", 'content': re.compile("MediaWiki", re.I)}):
+        try:
+            wiki = self.bangcommands["!wiki"](self, e, True, True)
+            title = wiki.output
+        except:
+            pass
+    elif page:
+        title = "Title: " + page.find('title').string
+        try:
+            meta_title = "Title: " + page.find('meta', attrs={'property': "og:title"}).get("content")
+        except:
+            meta_title = False
+        
+        
+    if meta_title:
+        return meta_title
+    else:
+        return title
+
+
+def last_title(self, e):
+    #displays the title of the last link posted (requires sql)
+    conn = sqlite3.connect("links.sqlite")
+    cursor = conn.cursor()
+    if (cursor.execute("SELECT url FROM links ORDER BY rowid DESC LIMIT 1")):
+        result = cursor.fetchone()
+        url = result[0]
+        e.input = url
+    conn.close()
+    
+    return url_posted(self, e, True)
+
+last_title.command = "!title"
+last_title.helptext = "Usage: !title\nShows the title of the last URL that was posted in the channel"
+
+
+    
+def last_link(self, e):
+    #displays the title of the last link posted (requires sql)
+    conn = sqlite3.connect("links.sqlite")
+    cursor = conn.cursor()
+    if (cursor.execute("SELECT url FROM links ORDER BY rowid DESC LIMIT 1")):
+        result = cursor.fetchone()
+        url = result[0]
+    conn.close()
+    e.output = url
+    return e
+
+last_link.command = "!lastlink"
+last_link.helptext = "Usage: !lastlink\nShows the last URL that was posted in the channel"
