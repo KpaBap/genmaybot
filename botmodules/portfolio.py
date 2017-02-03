486a43525b71935ab8895f47a4957c86fe3f8721
Dan Vu Quoc <dan@vuquoc.com>
2017-02-02 20:57:30 -0800

    Who the fuck picked this locale?

--- botmodules/portfolio.py
+++ botmodules/portfolio.py
@@ -2,7 +2,7 @@ import sqlite3, locale, urllib.request, urllib.error, urllib.parse
 try:
     locale.setlocale(locale.LC_ALL, 'English_United States')
 except:
-    locale.setlocale(locale.LC_ALL, 'en_US.utf8')
+    locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
 
 def portfolio(self, e):
 	
